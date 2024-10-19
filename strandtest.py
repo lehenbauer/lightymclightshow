8f98ac8fa05f75dd8f553365568370575a7b53f6
Karl Lehenbauer <karl@lehenbauer.com>
2024-10-19 02:14:56 +0000

    set strip type to RGB

--- strandtest.py
+++ strandtest.py
@@ -6,11 +6,11 @@
 # various animations on a strip of NeoPixels.
 
 import time
-from rpi_ws281x import PixelStrip, Color
+from rpi_ws281x import PixelStrip, Color, ws
 import argparse
 
 # LED strip configuration:
-LED_COUNT = 16        # Number of LED pixels.
+LED_COUNT = 300        # Number of LED pixels.
 LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -18,6 +18,7 @@ LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP_TYPE = ws.WS2811_STRIP_RGB
 
 
 # Define functions which animate LEDs in various ways.
@@ -84,6 +85,13 @@ def theaterChaseRainbow(strip, wait_ms=50):
                 strip.setPixelColor(i + q, 0)
 
 
+def goob():
+    # Create NeoPixel object with appropriate configuration.
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, strip_type=LED_STRIP_TYPE)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    return strip
+	
 # Main program logic follows:
 if __name__ == '__main__':
     # Process arguments
