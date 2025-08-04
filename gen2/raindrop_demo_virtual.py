4a36d6aa95a40687bbd390e61beb6b652933b91e
Karl Lehenbauer <karl@lehenbauer.com>
2025-08-04 04:19:13 +0000

    raindrop demo but using a virtual string

---
+++ gen2/raindrop_demo_virtual.py
@@ -0,0 +1,53 @@
+
+
+# load with ipython %load for now
+
+import time
+import sys
+import random
+
+from hardware import *
+from dispatcher import *
+from strip import Strip
+from virtual_strip import VirtualStrip
+
+physical_strip = initialize_strip()
+
+virtual_strip = VirtualStrip()
+virtual_strip.add_pixel_range(physical_strip, len(physical_strip) -1, 0)
+
+#strip = Strip(physical_strip)
+
+def run_demo(strip):
+    """Run the Pulsator demo sequence."""
+
+    # Create dispatcher and a timeline to hold our effect instances
+    dispatcher = Dispatcher()
+    timeline = Timeline()
+
+    # --- Instantiate all effects and store them on the timeline ---
+    timeline.gravity_filler = RaindropFill(virtual_strip)
+
+    # --- Schedule all actions ---
+
+    # 0.0s: Start the pulsator effect
+    dispatcher.schedule(0.0, lambda: dispatcher.run_background_effect(
+        timeline.gravity_filler.start(color=Color(255,255,255))
+        #timeline.gravity_filler.start()
+    ))
+
+    # --- Run the animation ---
+    # The dispatcher will now run until all scheduled events and effects are complete.
+    dispatcher.run()
+
+    print("Block fill demo complete.")
+    strip.blackout()
+
+
+if __name__ == "__main__":
+    try:
+        run_demo(virtual_strip)
+    except KeyboardInterrupt:
+        # Clean up and exit gracefully
+        print("\nKeyboard interrupt received. Turning off LEDs and exiting...")
+        strip.blackout()
